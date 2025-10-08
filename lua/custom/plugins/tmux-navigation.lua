55c7722f76f5f46fdb041b7c4aa12d1a9a814306
Fabio de Almeida <fabioalmeida2004@gmail.com>
2025-10-08 10:26:32 -0300

    kickstart customizations

---
+++ lua/custom/plugins/tmux-navigation.lua
@@ -0,0 +1,18 @@
+return { -- Tmux navigation
+  'christoomey/vim-tmux-navigator',
+  cmd = {
+    'TmuxNavigateLeft',
+    'TmuxNavigateDown',
+    'TmuxNavigateUp',
+    'TmuxNavigateRight',
+    'TmuxNavigatePrevious',
+    'TmuxNavigatorProcessList',
+  },
+  keys = {
+    { '<C-h>', '<cmd>TmuxNavigateLeft<cr>' },
+    { '<C-j>', '<cmd>TmuxNavigateDown<cr>' },
+    { '<C-k>', '<cmd>TmuxNavigateUp<cr>' },
+    { '<C-l>', '<cmd>TmuxNavigateRight<cr>' },
+    { '<C-\\>', '<cmd>TmuxNavigatePrevious<cr>' },
+  },
+}
