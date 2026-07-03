9349838fb3b971259cc757554dbe46601d9c5b3f
Fabio de Almeida <fabioalmeida2004@gmail.com>
2026-07-03 17:17:28 -0300

    tmux-sessionizer bind back

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,8 +9,7 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set('i', 'jk', '<Esc>')
-vim.keymap.set({ 'n', 'v', 'i' }, '<C-c>', '<Esc>')
+vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>', { desc = 'Open tmux sessionizer' })
 
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move selected lines up', silent = true })
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selected lines down', silent = true })
