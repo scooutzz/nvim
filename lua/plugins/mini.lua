c6af8ed04c0dc6016687ec34a623453640085891
Fabio de Almeida <fabioalmeida2004@gmail.com>
2026-07-15 17:09:13 -0300

    fix(mini): fix mini.nvim

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,11 +1,17 @@
 return {
   {
-    'echasnovski/mini.nvim',
+    'nvim-mini/mini.nvim',
     dependencies = {
       'nvim-tree/nvim-web-devicons',
     },
     config = function()
-      require('mini.ai').setup { n_lines = 500 }
+      require('mini.ai').setup {
+        mappings = {
+          around_next = 'aa',
+          inside_next = 'ii',
+        },
+        n_lines = 500,
+      }
       require('mini.surround').setup()
     end,
   },
