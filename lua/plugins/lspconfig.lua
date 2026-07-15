fbf9b9c03e08ad34387b47a9118b5fb13d14e4dd
Fabio de Almeida <fabioalmeida2004@gmail.com>
2026-07-15 14:27:51 -0300

    refactor(keymaps): better keymaps

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -19,55 +19,31 @@ return {
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('UserLspConfig', { clear = true }),
         callback = function(event)
-          vim.keymap.set('n', 'gR', '<cmd>Telescope lsp_references<CR>', { desc = 'Show LSP [R]eferences' })
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = '[G]o to [D]eclaration' })
-          vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<CR>', { desc = 'Show LSP [d]efinitions' })
-          vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', { desc = 'Show LSP [i]mplementations' })
-          vim.keymap.set('n', 'gt', '<cmd>Telescope lsp_type_definitions<CR>', { desc = 'Show LSP [t]ype definitions' })
-          vim.keymap.set({ 'n', 'v' }, '<leader>vca', vim.lsp.buf.code_action, { desc = 'See available [c]ode [a]ctions' })
-          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Smart [r]ename' })
+          local telescope = require 'telescope.builtin'
+          local map = function(keys, func, desc, mode)
+            mode = mode or 'n'
+            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          end
 
-          vim.keymap.set('n', '<leader>dq', vim.diagnostic.setqflist, { desc = 'Workspace [d]iagnostics (Quickfix)' })
-          vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, { desc = 'Buffer diagnostics (Location List)' })
-
-          vim.keymap.set('n', 'df', vim.diagnostic.open_float, { desc = 'Show line diagnostics' })
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Show documentation for what is under cursor' })
-          vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, { desc = 'Show signature help' })
+          map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+          map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
+          map('grr', telescope.lsp_references, '[G]oto [R]eferences')
+          map('gri', telescope.lsp_implementations, '[G]oto [I]mplementations')
+          map('grd', telescope.lsp_definitions, '[G]oto [D]efinitions')
+          map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+          map('gO', telescope.lsp_document_symbols, 'Open Document Symbols')
+          map('gW', telescope.lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
+          map('grt', telescope.lsp_type_definitions, '[G]oto [T]ype Definition')
+          map('<C-h>', vim.lsp.buf.signature_help, 'Show signature help', 'i')
 
           local client = vim.lsp.get_client_by_id(event.data.client_id)
-
           if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-            vim.keymap.set('n', '<leader>th', function()
+            map('<leader>th', function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-            end, { desc = '[T]oggle inlay [H]ints' })
+            end, '[T]oggle inlay [H]ints')
           end
         end,
       })
-
-      vim.diagnostic.config {
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = ' ',
-            [vim.diagnostic.severity.WARN] = ' ',
-            [vim.diagnostic.severity.HINT] = '󰠠 ',
-            [vim.diagnostic.severity.INFO] = ' ',
-          },
-        },
-        virtual_text = true,
-        underline = true,
-        update_in_insert = false,
-        float = {
-          focusable = false,
-          style = 'minimal',
-          border = 'rounded',
-          source = true,
-        },
-      }
-
-      vim.keymap.set('n', '<leader>lx', function()
-        local current = vim.diagnostic.config().virtual_text
-        vim.diagnostic.config { virtual_text = not current }
-      end, { desc = 'Toggle LSP virtual text' })
 
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = require('blink-cmp').get_lsp_capabilities(capabilities)
@@ -113,6 +89,35 @@ return {
         'emmet_ls',
         'lua_ls',
       }
+
+      vim.diagnostic.config {
+        signs = vim.g.have_nerd_font and {
+          text = {
+            [vim.diagnostic.severity.ERROR] = '󰅚 ',
+            [vim.diagnostic.severity.WARN] = '󰀪 ',
+            [vim.diagnostic.severity.INFO] = '󰋽 ',
+            [vim.diagnostic.severity.HINT] = '󰌶 ',
+          },
+        } or {},
+        virtual_text = true,
+        underline = { severity = vim.diagnostic.severity.ERROR },
+        update_in_insert = false,
+        float = {
+          focusable = false,
+          style = 'minimal',
+          border = 'rounded',
+          source = true,
+        },
+      }
+
+      vim.keymap.set('n', '<leader>dq', vim.diagnostic.setqflist, { desc = 'Workspace [d]iagnostics (Quickfix)' })
+      vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, { desc = 'Buffer diagnostics (Location List)' })
+      vim.keymap.set('n', 'df', vim.diagnostic.open_float, { desc = 'Show line diagnostics' })
+
+      vim.keymap.set('n', '<leader>lx', function()
+        local current = vim.diagnostic.config().virtual_text
+        vim.diagnostic.config { virtual_text = not current }
+      end, { desc = 'Toggle LSP virtual text' })
     end,
   },
 }
